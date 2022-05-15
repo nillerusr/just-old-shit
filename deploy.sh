2c8eb17969285effa4bdfdc098368c8e30e67976
nillerusr <nillerusr@users.noreply.github.com>
2022-05-16 00:10:21 +0300

    shit

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt install -y make unzip python3 ccache imagemagick openjdk-8-jdk openjdk-8-jre ant-contrib > /dev/null
-wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip > /dev/null
+sudo apt install -y make unzip python3 ccache imagemagick openjdk-8-jdk openjdk-8-jre ant-contrib
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip -o /dev/null
 unzip android-ndk-r10e-linux-x86_64.zip > /dev/null
 mv android-ndk-r10e ndk/
 export ANDROID_NDK_HOME=$(pwd)/ndk

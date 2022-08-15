d49e0a1e3bbaf22979e4ae0cf3e94771dfff68f1
nillerusr <nillerusr@gmail.com>
2022-08-15 23:33:16 +0300

    test

--- deploy.sh
+++ deploy.sh
@@ -56,7 +56,7 @@ git clone --depth 1 https://github.com/nillerusr/srceng-mod-launcher
 	fi
 
 	ant debug &&
-	sshpass -p $SSH_PASS scp bin/srcmod-debug.apk nillerusr@nillerusr.fvds.ru:/var/www/html/c4mf4stin3/$MOD_NAME-$MOD_VER.apk
+	sshpass -p $SSH_PASS scp -o StrictHostKeyChecking=no bin/srcmod-debug.apk nillerusr@nillerusr.fvds.ru:/var/www/html/c4mf4stin3/$MOD_NAME-$MOD_VER.apk
 	../scripts/send-to-discord.py $3 build test - http://nillerusr.fvds.ru/c4mf4stin3/$MOD_NAME-$MOD_VER.apk
 
 #	rm -rf gen bin lib assets
